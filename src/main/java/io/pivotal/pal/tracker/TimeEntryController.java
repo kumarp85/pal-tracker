e1225119583ab9d345969fa150798111a634750e
kumarp85 <mana.santosh9991@gmail.com>
2019-08-20 17:31:48 +0530

    Update TimeEntryController.java

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -2,12 +2,13 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/time-entries")
 public class TimeEntryController {
 
     private final TimeEntryRepository repository;
@@ -16,14 +17,14 @@ public class TimeEntryController {
     }
 
 
-    @RequestMapping(value ="/time-entries", method = RequestMethod.POST)
-    public ResponseEntity<TimeEntry> create(TimeEntry timeEntry) {
+    @PostMapping
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
         TimeEntry response = repository.create(timeEntry);
         return new ResponseEntity<TimeEntry>(response, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value ="/time-entries", method = RequestMethod.GET)
-    public ResponseEntity<TimeEntry> read(Long id) {
+    @GetMapping("{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
         TimeEntry response = repository.find(id);
         if(response != null) {
             return new ResponseEntity<TimeEntry>(response, HttpStatus.OK);
@@ -32,14 +33,14 @@ public class TimeEntryController {
         }
     }
 
-    @RequestMapping(value ="/time-entries", method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
         List<TimeEntry> timeEntryResponse = repository.list();
         return new ResponseEntity<List<TimeEntry>>(timeEntryResponse, HttpStatus.OK);
     }
 
-    @RequestMapping(value ="/time-entries", method = RequestMethod.PUT)
-    public ResponseEntity<TimeEntry> update(long id, TimeEntry timeEntry) {
+    @PutMapping("{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable long id, @RequestBody TimeEntry timeEntry) {
         TimeEntry response = repository.update(id, timeEntry);
         if(response != null) {
             return new ResponseEntity<TimeEntry>(response, HttpStatus.OK);
@@ -48,8 +49,8 @@ public class TimeEntryController {
         }
     }
 
-    @RequestMapping(value ="/time-entries", method = RequestMethod.DELETE)
-    public ResponseEntity<?> delete(long id) {
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable long id) {
         repository.delete(id);
         TimeEntry responseEntry = repository.find(id);
         if(responseEntry == null) {
